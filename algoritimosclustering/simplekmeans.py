2f5bef282bf790d1b6d55b98660d0653b031fddf
SyaneAndrade <syane_andrade@outlook.com>
2018-08-31 01:50:00 -0300

    Mudanças de parametros para ver se funcionava o kmeans com ds

--- algoritimosclustering/simplekmeans.py
+++ algoritimosclustering/simplekmeans.py
@@ -6,10 +6,11 @@ class SimpleKmeans(object):
     clusters  = []
     centers = None
     labels = []
+    T = None
 
 
     def __init__(self, num_cluster):
-        self.kmeans = KMeans(n_clusters=num_cluster, init='random')
+        self.kmeans = KMeans(n_clusters=num_cluster, init='random', max_iter=1000)
         self.n_cluster = num_cluster
 
     def aplica_kmeans(self, dados):
@@ -17,11 +18,13 @@ class SimpleKmeans(object):
         if (self.labels is None):
             self.labels = self.kmeans.labels_
         else:
-            self.labels = np.append(self.kmeans.labels_, self.labels)
+            self.labels = np.append(self.labels, self.kmeans.labels_)
         self.centers = self.kmeans.cluster_centers_
+        self.T = self.kmeans.cluster_centers_.T
 
     def atualiza_kmeans(self, dados):
-        self.kmeans = KMeans(n_clusters=self.n_cluster, init=self.centers)
+        # self.kmeans = KMeans(n_clusters=self.n_cluster, init=self.centers)
+        self.kmeans = KMeans(n_clusters=self.n_cluster, init=self.centers, max_iter=1000, n_init=1)
         self.aplica_kmeans(dados)
 
         
