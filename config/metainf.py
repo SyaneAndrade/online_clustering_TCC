4b04233f9fe5d7b679ca014f95a24aad323a7328
Syane <syane_andrade@outlook.com>
2019-04-16 18:38:39 -0300

    configuração para o conjunto de dados magic

--- config/metainf.py
+++ config/metainf.py
@@ -6,14 +6,14 @@ dados = "P"
 results = "GT"
 
 # lista = ["constraceptive", "german", "optic", "pageblocks", "satellite", "magic", "yeast"]
-lista = ["magic","yeast"]
+lista = ["magic"] #, "yeast"]
 
 # numCluster = [3, 2, 2, 10, 5, 7, 10]
 
-numCluster = [7,10]
+numCluster = [7] #, 10]
 
 numPart = 1
 
-threshholdBirch = [0.47, 0.27]
+threshholdBirch = [ 0.209] #0.2037821906368138120]
 
-threshholdLeader = [2.18, 1.1]
+threshholdLeader = [0.95]#, 0.68]
