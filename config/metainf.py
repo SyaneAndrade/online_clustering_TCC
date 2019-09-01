eb80d713e99442c6d39f7c5d861d18047f01375b
Guilherme Oliveira <guilhermeborgesoliveira@gmail.com>
2019-09-01 13:43:30 -0300

    Implementando objeto auxiliar para guardar as metas

--- config/metainf.py
+++ config/metainf.py
@@ -17,3 +17,23 @@ numPart = 1
 threshholdBirch = [0.25]  #0.209] #0.2037821906368138120]
 
 threshholdLeader = [2.1]#0.95]#, 0.68]
+
+valoresParaAlgoritmos = {
+  "constraceptive": Info("constraceptive", 0, 0, 3),
+  "german": Info("german", 0, 0, 2),
+  "optic": Info("optic", 0, 0, 10),
+  "pageblocks": Info("pageblocks", 0, 0, 5),
+  "satellite": Info("satellite", 0, 0, 7),
+  "magic": Info("magic", 0, 0, 2),
+  "yeast": Info("yeast", 0, 0, 10)
+}
+
+
+class Info:
+  def __init__(self, name, threshholdBirch, threshholdLeader, numCluster):
+    self.name = name
+    self.threshholdBirch = threshholdBirch
+    self.threshholdLeader = threshholdLeader
+    self.numCluster = numCluster
+
+
