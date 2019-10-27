4e0f31e12507ad2aa578c1424244282f194c85b7
Guilherme Oliveira <guilhermeborgesoliveira@gmail.com>
2019-10-27 20:08:10 -0300

    Adicionando comentário com o fluxo que eu estava tentando fazer

--- main.py
+++ main.py
@@ -73,6 +73,26 @@ def main():
         print(f'DATASET: {key}')
         caminhoDoDataset = "dados/mock/" + key +"/"+ key + dados + ".csv"
         encontraValoresDasVariaveis(valoresParaAlgoritmos[key])
+   # for index in range(len(lista)):
+    #     # Caminho onde se encontra o conjunto de dados
+    #     caminho = "dados/mock/" + lista[index] +"/"+ lista[index] + dados + ".csv"
+    #     print(lista[index])
+    #     # Cria um objeto do tipo gerenciador já inicializando os parametros necessários
+    #     gerenciador = Gerenciador(caminho)
+    #     # Numero de cluster para os algoritimos de clusterização de dados
+    #     num_cluster = numCluster[index]
+    #     tsbirch = threshholdBirch[index]
+    #     tsLeader = threshholdLeader[index]
+    #     # Cria um df dos dados retirados do csv
+    #     gerenciador.inicia(num_cluster, tsbirch, tsLeader)
+    #     gerenciador.iniciaDataset(numPart)
+    #     # Aplica o algoritimo kmenas nos dados
+    #     gerenciador.iniciar(False)
+    #     plotGraficoClustering(gerenciador.simple_kmeans.labels, gerenciador.simple_kmeans.centers, "Kmeans sklearn", gerenciador.daoIO.randon_dados)
+    #     plotGraficoClustering(gerenciador.sp_kmeans.labels, gerenciador.sp_kmeans.centers, "Kmeans online mode make for me", gerenciador.daoIO.randon_dados)
+    #     plotGraficoClustering(gerenciador.birch.labels, gerenciador.birch.centers, "BIRCH", gerenciador.daoIO.randon_dados)
+    #     plotGraficoClustering(gerenciador.leader.labels, gerenciador.leader.centers, "The Leader Algorithm", gerenciador.daoIO.randon_dados)
+    #     gerenciador.criarCluster(False)
 
 
         while(gerenciador.executa):
