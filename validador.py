1aaa21adc20f667fea1643688dcf3bc03482e1f8
Jessiane Andrade <syane_andrade@outlook.com>
2021-06-07 19:29:57 -0300

    Adicionando resultados

--- validador.py
+++ validador.py
@@ -13,9 +13,9 @@ def readCsv(path, head):
     return pd.read_csv(path, header=head)
 
 def gera_estatisticas(dataset, algo, estastisticas_gerais):
-    caminho = "dados/mock/" + dataset + "/"
-    path_verdade = caminho + dataset + "GT.csv"
-    path_particao =  caminho + algo + "_" + dataset + "/" + algo + "_" + dataset + "_labels.csv"
+    caminho_resultados = "dados/mock/" + dataset + "/"
+    path_verdade = caminho_resultados + dataset + "GT.csv"
+    path_particao =  caminho_resultados + algo + "_" + dataset + "/" + algo + "_" + dataset + "_labels.csv"
     contagem_de_pares = ContagemParesOnline(dataset + '_labels_verdadeiro', algo)
     labels_verdadeiro = readCsv(path_verdade, None).to_numpy()
     labels_particao = readCsv(path_particao, None).to_numpy()
@@ -35,9 +35,11 @@ def gera_estatisticas(dataset, algo, estastisticas_gerais):
         df.append(dados)
     dados_rand_index = pd.DataFrame(list(map(np.ravel, rand_index)))
     dados_estatististicos = pd.DataFrame(list(map(np.ravel, df)))
-    nomeArquivo =  algo + "_" + dataset + "/" + algo + "_" + dataset + "_ri"
+    caminho = "resultados/" + dataset + "/" + algo + "_" + dataset + "/"
+    os.makedirs(os.path.dirname(caminho), exist_ok=True)
+    nomeArquivo =  algo + "_" + dataset + "_ri"
     dados_rand_index.to_csv(caminho + nomeArquivo + '.csv', index=False, header=False)
-    nomeArquivo_dados = algo + "_" + dataset + "/" + algo + "_" + dataset + "_estastisticas"
+    nomeArquivo_dados = algo + "_" + dataset + "_estastisticas"
     dados_estatististicos.to_csv(caminho + nomeArquivo_dados + '.csv', index=False, header=['rand_index', 'media', 'desvio_padrao'])
     print("RI Python {} {}: {}".format(dataset, algo, sk_rand_score))
     print("N's Python {} {}: \n{}".format(dataset, algo, sk_pair_confusion_matrix))
@@ -53,7 +55,7 @@ def gera_graph(dataset, column):
     dict = {}
     list_algo = ['simple_pass_k_means', 'leader', 'birch', 'bok', 'boem', 'marjorityvoting']
     for algo in list_algo:
-        caminho = "dados/mock/" + dataset + "/" + algo + "_" + dataset + "/" +  algo + "_" + dataset + "_estastisticas.csv"
+        caminho = "resultados/" + dataset + "/" + algo + "_" + dataset + "/" +  algo + "_" + dataset + "_estastisticas.csv"
         sp_k_means = readCsv(caminho, 0)[column]
         sp_k_means_ar = sp_k_means.to_numpy().flatten()
         dict[algo] = sp_k_means_ar
@@ -61,10 +63,10 @@ def gera_graph(dataset, column):
     df = dados.stack().reset_index().rename(columns={"level_0": "time", "level_1": "clustering", 0: column})
     df_pivot = df.pivot("time",'clustering', column)
     sns.relplot(x='time', y=column, hue='clustering', style='clustering', kind='line', data=df)
-    imagens = "dados/mock/" + dataset + "/" + dataset + "_graficos/"
+    imagens = "resultados/" + dataset + "/" + dataset + "_graficos/"
     os.makedirs(os.path.dirname(imagens), exist_ok=True)
     plt.savefig(imagens + database + "_" + column + ".jpg")
-    plt.show()
+    # plt.show()
 
 if __name__ == '__main__':
     list_databases = ["constraceptive", "german", "optic", "pageblocks", "satellite", "magic", "yeast"]
@@ -77,7 +79,7 @@ if __name__ == '__main__':
         for algo in list_algo:
            gera_estatisticas(database, algo, estastisticas_gerais)
     estastisticas_gerais = pd.DataFrame(list(map(np.ravel, estastisticas_gerais)))
-    estastisticas_gerais.to_csv('estatisticas_gerais.csv', index=False, header=columns)
+    estastisticas_gerais.to_csv('resultados/estatisticas_gerais.csv', index=False, header=columns)
     
     columns_graph = ['rand_index', 'media', 'desvio_padrao']
     for database in list_databases:
